309be58e45f80ce97582cac501679760974ac683
Alex T Leach <alextleach@gmail.com>
2016-07-26 16:56:47 -0500

    having lots of errors. stuck and cant move forward at the moment

--- app/Http/routes.js
+++ app/Http/routes.js
@@ -15,10 +15,21 @@
 | Route.resource('user', 'UserController')
 */
 
-const Route = use('Route')
+const Route = use('Route');
+const Photo = use('App/Model/Photo');
 
 Route.on('/').render('welcome')
 
 Route.get('/photos', function * (req, res){
-  yield res.sendView('photos');
+  const { url, caption } = yield req.session.all();
+  yield res.sendView('photos',{url,caption});
 });
+
+Route.post('/photos', function * (req, res){
+  const Database = use('Database');
+  const url = req.input('url');
+  const caption = req.input('caption');
+  const photo = yield Photo.create({ url, caption });
+  yield req.session.put({url, caption});
+  res.redirect('/photos');
+})
